99ea49a37c154095646240c80e08d476220da533
Vincent Vanwaelscappel <vincent@cubedesigners.com>
2026-05-19 19:48:59 +0200

    fix : boucle infinie sur profile

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -1,13 +1,14 @@
+import { useMemo } from 'react';
 import { authService } from '../services/auth.service';
-import {AuthResponse} from '../types';
+import { AuthResponse } from '../types';
 
 interface AuthContext {
-  user: AuthResponse|null;
+  user: AuthResponse | null;
   token: string | null;
 }
 
 export function useAuth(): AuthContext {
   const user = authService.getCurrentUser();
   const token = authService.getToken();
-  return { user, token };
+  return useMemo(() => ({ user, token }), [user?.id, token]);
 }
