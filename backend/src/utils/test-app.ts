2b1b7f70d869a43ef9217f318a479ebff8890c4d
Vincent Vanwaelscappel <vincent@cubedesigners.com>
2026-06-05 13:43:59 +0200

    backend tests : contolleur auth, error handler

---
+++ backend/src/utils/test-app.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import routes from '../routes';
+import {errorHandler} from '../middleware/errorHandler';
+
+export const createTestApp = () => {
+    const app = express();
+    app.use(express.json());
+    app.use(routes);
+    app.use(errorHandler);
+    return app;
+};
